7d550eec0665aaeb74afb71493043e64889569b3
zfkun <zfkun@msn.com>
2013-11-06 23:12:42 +0800

    update for `saber-x`
    update for `saber-x` with `jsdoc 3`

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,6 @@
 /*global module:false*/
 module.exports = function (grunt) {
-
+    
     grunt.initConfig({
     
         meta: {
@@ -11,25 +11,69 @@ module.exports = function (grunt) {
         },
 
         jsdoc: {
-            options: {
-                configure: '.jsdocrc',
-                destination: 'doc'
+            'saber-lang': {
+                options: {
+                    configure: '.jsdocrc',
+                    destination: 'doc/saber-lang'
+                },
+                src: [ '<%= meta.src.main %>/saber-lang/src/*.js' ]
             },
-            files: [
-                '<%= meta.src.main %>/saber-lang/src/*.js',
-                '<%= meta.src.main %>/saber-emitter/src/*.js',
-                '<%= meta.src.main %>/saber-dom/src/*.js',
-                '<%= meta.src.main %>/saber-ui/src/*.js',
-                '<%= meta.src.main %>/saber-control/src/*.js',
-                '<%= meta.src.main %>/saber-button/src/*.js'
-            ]
+            'saber-string': {
+                options: {
+                    configure: '.jsdocrc',
+                    destination: 'doc/saber-string'
+                },
+                src: [ '<%= meta.src.main %>/saber-string/src/*.js' ]
+            },
+            'saber-emitter': {
+                options: {
+                    configure: '.jsdocrc',
+                    destination: 'doc/saber-emitter'
+                },
+                src: [ '<%= meta.src.main %>/saber-emitter/src/*.js' ]
+            },
+            'saber-dom': {
+                options: {
+                    configure: '.jsdocrc',
+                    destination: 'doc/saber-dom'
+                },
+                src: [ '<%= meta.src.main %>/saber-dom/src/*.js' ]
+            },
+            'saber-ui': {
+                options: {
+                    configure: '.jsdocrc',
+                    destination: 'doc/saber-ui'
+                },
+                src: [ '<%= meta.src.main %>/saber-ui/src/*.js' ]
+            },
+            'saber-control': {
+                options: {
+                    configure: '.jsdocrc',
+                    destination: 'doc/saber-control'
+                },
+                src: [ '<%= meta.src.main %>/saber-control/src/*.js' ]
+            },
+            'saber-button': {
+                options: {
+                    configure: '.jsdocrc',
+                    destination: 'doc/saber-button'
+                },
+                src: [ '<%= meta.src.main %>/saber-button/src/*.js' ]
+            },
+            'saber-tab': {
+                options: {
+                    configure: '.jsdocrc',
+                    destination: 'doc/saber-tab'
+                },
+                src: [ '<%= meta.src.main %>/saber-tab/src/*.js' ]
+            }
         }
 
     });
 
 
-    grunt.loadNpmTasks('grunt-jsdoc');
+    grunt.loadNpmTasks( 'grunt-jsdoc' );
 
-    grunt.registerTask('doc', ['jsdoc']);
+    grunt.registerTask( 'doc', ['jsdoc']);
 
 };
